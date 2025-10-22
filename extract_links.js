624c5e0e5256df516b5bb6934566f74842f47db0
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-10-22 06:23:08 +0000

    chore: bump version from 2.4 to 2.5

--- extract_links.js
+++ extract_links.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Extract All Posted Links
 // @namespace    http://tampermonkey.net/
-// @version      2.4
+// @version      2.5
 // @updateURL    https://github.com/Garcarius/forumslinkgraber/raw/main/extract_links.js
 // @downloadURL  https://github.com/Garcarius/forumslinkgraber/raw/main/extract_links.js
 // @description  Adds a button to extract all posted links (ignoring unwanted ones) and handles redirects. Now includes options to download or copy links to clipboard, with enhanced UI and local storage support to avoid duplicates.
